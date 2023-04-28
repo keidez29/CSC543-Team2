cfa254070daaf72c50275c83c9a62f15643238c9
Au4Ob10 <agranchelli27@gmail.com>
2023-04-28 16:49:30 -0400

    video thumbnails wip

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-const uploadButton = document.getElementById("upload-button")
-let uploadScreen = document.getElementById("upload-video-screen")
+// const uploadButton = document.getElementById("upload-button")
+// let uploadScreen = document.getElementById("upload-video-screen")
+
+const thumbnailUploadButton = document.getElementById("thumbnail-upload")
 
 
-let showUploadScreen = () => {
 
-if (uploadScreen.style.display="none") {
-
-        uploadScreen.style.display="flex"
-    }
-
-else {
-    uploadScreen.style.display="none"
-}
-}
-
-uploadButton.addEventListener("click",showUploadScreen)
