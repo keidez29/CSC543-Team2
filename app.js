353929d1dd583654388c0088c4524e84a8b3134b
Valon Ajeti <ajetiv1@southernct.edu>
2023-05-06 13:45:55 -0400

    dos

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('homepage.html').pipe(res)
+})
+server.listen(80);
