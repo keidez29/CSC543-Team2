da906b914359def88e53d4c185509f8cf76983f8
ajetiv1 <ajetiv1@southernct.edu>
2023-05-07 16:25:31 -0400

    working version 1

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-const http = require('http')
-const fs = require('fs')
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('homepage.html').pipe(res)
-})
-server.listen(80);
+app.use(express.static('public_html'))
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/public_html/homepage.html'));
+});
+
+app.use('/', router);
+
+router.get('/search',function(req,res){
+  res.sendFile(path.join(__dirname+'/public_html/search-page.html'));
+});
+ 
+router.get('/upload',function(req,res){
+  res.sendFile(path.join(__dirname+'/public_html/upload.html'));
+});
+
+router.get('/login',function(req,res){
+  res.sendFile(path.join(__dirname+'/public_html/user-login.html'));
+});
+
+app.listen(80, () => console.log('Application is running'));
