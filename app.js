3a982ce6d66d56cbbb877db174813da867b52fe8
Au4Ob10 <agranchelli27@gmail.com>
2023-05-12 20:17:50 -0400

    final version

--- app.js
+++ app.js
@@ -1,56 +1,18 @@
+
+    /* Team 2 - Keila Hernandez, Austin Granchelli, Donte Brooks, Valon Ajeti
+    (Final Project)
+    This is our app.js file.  Using express, this file communicates with the server
+    when a file is submitted.  The file is then stored in the "Videos" directory. */
+
+
+
 const express = require('express');
 const upload = require('express-fileupload')
 const app = express();
 const path = require('path');
 const router = express.Router();
-const multer = require('multer');
-
-
 
 app.use(express.static('public_html'))
-
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/public_html/homepage.html'));
-});
-
-app.use('/', router);
-
-router.get('/search',function(req,res){
-  res.sendFile(path.join(__dirname+'/public_html/search-page.html'));
-});
- 
-router.get('/upload',function(req,res){
-  res.sendFile(path.join(__dirname+'/public_html/upload.html'));
-});
-
-router.get('/login',function(req,res){
-  res.sendFile(path.join(__dirname+'/public_html/user-login.html'));
-});
-
-/*
-let qObj = {name: document.getElementById("userName").value,
-password: document.getElementById("password").value
-};
-let AJAX = new XMLHttpRequest(); //create AJAX object
-AJAX.onerror = function() { //attach error event handler
-alert("Network error");
-}
-AJAX.onload = singnupOnload; //attach load event handler
-//setup the connection parameters
-AJAX.open("POST","http://34.75.141.24/upload-vid");
-AJAX.setRequestHeader("Content-type", "application/json");
-AJAX.send(queryObjectToString(qObj));
-
-*/
-// app.listen(80, () => console.log('Application is running'));
-
-
-// Keila Hernandez & Austin Granchelli
-// CSC 543 - HW6
-// 12 April 2023
-// Description: Main Application
-
-
 
 app.use(upload())
 
@@ -59,17 +21,6 @@ app.use(express.static("/public_html/css"))
 
     console.log("Listening on port 80.")
 
-// const server = http.createServer((req, res) => {
-//  res.writeHead(200, { 'content-type': 'text/html' })
-//  fs.createReadStream('homepage.html').pipe(res)
-// })
-// server.listen(80);
-// app.get( "/",(req,res) =>{
-// const server = http.createServer((req, res) => {
-//  res.writeHead(200, { 'content-type': 'text/html' })
-//  fs.createReadStream('homepage.html').pipe(res)
-// })
-// server.listen(80);
 app.get( "/",(req,res) =>{
 
     res.sendFile(__dirname + '/public_html/homepage.html') })
@@ -77,7 +28,6 @@ app.get( "/",(req,res) =>{
 
 app.post('/', (req,res)=> {
     if (req.files) {
-
 
         let file = req.files.file
         let fileName = file.name
